8c20f5a806d0658ddbc84d7248dc0dd8aa4e56a8
Alana Dellani <alanadellani@outlook.com>
2026-03-24 20:49:22 -0300

    minhaListaAPI.

--- App.js
+++ App.js
@@ -1,54 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { FlatList, TextInput } from 'react-native-web';
 
 export default function App() {
+  const [dados, setDados] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  const buscarDados = async () => {
+    try {
+      const resposta = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const dadosr = await resposta.json();
+      setDados(dadosr);
+    } catch (erro) {
+      console.error('Erro ao buscar dados:', erro);
+    } finally {
+      setLoading(false);
 
-  const [nome, setNome] = useState(' ');
-  const [minhaLista, setMinhaLista] = useState([]);
-
-  function adicionarContato() {
-
-    const novoContato = {
-      id: Math.random().toString(),
-      nome: nome
     }
-    setMinhaLista([novoContato, ...minhaLista]);
-    setNome('')
-
   }
 
+  useEffect(() => {
+    buscarDados();
 
-  return (
-    <View style={styles.container}>
-      <Text>Meus contatos</Text>
+  }, []);
 
-      <TextInput>
-        placeholder='Digite seu nome'
-        value={nome}
-        onChangeText={setNome}
-      </TextInput>
+  if (loading) {
+    return (
+      <View style={StyleSheet.container}>
+        <Text>Carregando...</Text>
 
-      <button>
-        title='Adicionar'
-        onPress= {adicionarContato}
-      </button>
+      </View>
+    )
+  };
 
+return (
+  <View style={styles.container}>
+    <FlatList
+      data={dados}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+        <View style={styles.card}>
+          <Text style={styles.body}>{item.title}</Text>
+          <Text>{item.body}</Text>
 
-
-      <FlatList>
-        data={minhaLista}
-        keyExtractor={item => item.ed.toSting()}
-        renderItem={({ item, index }) => <Text>{item.nome}</Text>}
-
-      </FlatList>
-
-    </View>
-
-  )
+        </View>
+      )} />
+  </View>
+);
 }
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop:20,
+    paddingHorizontal: 10,
+  },
+  card : {
+    backgroundColor: '#f2f2f2',
+      paddingTop: 10,
+      marginBottom: 10,
+      borderRadius: 5
 
-})
+  },
+  titulo :{
+    fontWeight: 'bold',
+    marginBottom: 10
+  }
+
+});
